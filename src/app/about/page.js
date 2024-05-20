c281a3f04a1cf32aa4d18d725ae0d5dbf28cdb9f
Samuel Draper <samueledraper@outlook.com>
2024-05-20 16:16:33 +0100

    Add about

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,7 +1,83 @@
 export default function About() {
   return (
     <main>
-      <section></section>
+      <section>
+        <p>
+          Throughout our time in education, as primary teachers and Head
+          Teachers, our strong belief has always been that children who are
+          happy and comfortable learn best. Therefore, we plan individual
+          lessons to engage your child and foster a resilient and reflective
+          attitude to learning.
+        </p>
+      </section>
+      <section>
+        <div>
+          <h3>Jane</h3>
+          <p>
+            I have taught for over 25 enjoyable years, working in both the state
+            and independent sectors in many different roles. These have included
+            leading English (including phonics) and assessment across the
+            school; teaching in mainly Key Stage One and Lower Key Stage Two
+            classes; supporting children one-to-one and in small 'booster'
+            groups and as Joint Head Teacher.
+          </p>
+          <p>
+            I graduated with a BEd (Hons) in teaching and have continued to
+            study, completing courses on dyslexia, phonics teaching,
+            multi-sensory maths, ADHD, sensory processing, ASD, improving
+            children's writing and dyscalculia. I have recently completed my
+            Level 3 Certificate in Practical Solutions for Dyslexia, with the
+            British Dyslexia Association. I ensure I keep up to date with the
+            latest research by attending conferences, webinars and exhibitions.
+          </p>
+          <p>
+            Since beginning tutoring, I have had the opportunity to follow my
+            interest in SEN and work with children in small groups and
+            one-to-one. This enables strong working relationships to be built
+            quickly and the child’s own interests incorporated into the
+            teaching, increasing the child's confidence which, in turn, enables
+            them to make rapid progress and become a more independent learner.
+            It has enabled me to achieve what I set out to do when starting
+            teaching initially - to make a difference to individual children and
+            see their confidence and independence grow!
+          </p>
+        </div>
+        <div>
+          <h3>Yamin</h3>
+          <p>
+            I have 13 years of teaching experience in Key Stage Two in a local
+            independent school, where I went on to lead mathematics and
+            assessment and become Joint Head Teacher. After graduating with a BA
+            Hons in Home Economics and Religious Education, I pursued a career
+            in marketing before setting up my own catering business. Having two
+            daughters and volunteering in their school, furthered my interest in
+            education and how children learn, and at this point I retrained and
+            achieved Qualified Teacher Status.
+          </p>
+          <p>
+            I have taught all subjects across the Key Stage Two curriculum
+            including verbal reasoning, English and mathematics to support
+            pupils sitting the Transfer Test (11+). Specialising in mathematics,
+            I also helped prepare pupils for SATs as well as independent school
+            entrance exams and interview techniques. I have continued to teach
+            mathematics to smaller groups of pupils, as well as on a one-to-one
+            basis, to build children's confidence and develop their mathematical
+            skills.
+          </p>
+          <p>
+            I have mentored trainee teachers, provided maths training for class
+            teachers and for parents, to understand how maths is taught in
+            schools. Throughout my teaching career I have continued to study,
+            completing courses on mastery in mathematics and deeper learning
+            across the curriculum including in English, dyslexia, autism and
+            ADHD.
+          </p>
+          <p>
+            I thoroughly enjoy teaching and there are few things better than
+            seeing the reaction of a child when they have that 'Eureka' moment.
+          </p>
+        </div>
+      </section>
     </main>
   );
 }
