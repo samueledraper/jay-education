63ebac695ba6f33f79877b8093987c6920586117
Samuel Draper <samueledraper@outlook.com>
2024-05-29 11:03:42 +0100

    Add and style homepage content

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,42 @@
+import Image from "next/image";
+
 import UpcomingWorkshops from "@/components/UpcomingWorkshops";
 import TuitionInfo from "@/components/TuitionInfo";
 import Testimonials from "@/components/Testimonials";
 
+import hero from "../../public/images/tuition-session-reading.jpg";
+
 export default function Home() {
   return (
     <main>
+      <section>
+        <div className="w-10/12 mx-auto max-w-4xl my-16 lg:my-32 text-xl md:text-2xl lg:text-3xl">
+          <h2 className="mb-8 leading-relaxed">
+            Our strong belief has always been that children who are happy and
+            comfortable learn best.
+          </h2>
+          <h2 className="mb-8 leading-relaxed">
+            Children who have a good attitude to learning, are resilient and
+            reflective learners, achieve well and therefore building confidence
+            for learning underpins all our teaching.
+          </h2>
+        </div>
+
+        <div className="relative mx-auto h-96 md:h-128">
+          <Image
+            quality={100}
+            src={hero}
+            alt={""}
+            fill
+            style={{
+              objectFit: "cover",
+            }}
+          />
+        </div>
+      </section>
+
       <UpcomingWorkshops />
+
       <TuitionInfo />
       <Testimonials />
     </main>
