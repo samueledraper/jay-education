eaaa70368544cfe3357be6962383f463fbcb9be2
Samuel Draper <samueledraper@outlook.com>
2024-05-29 11:06:43 +0100

    Add and style footer content

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,74 +5,102 @@ import facebook from "@/icons/facebook-logo-primary.png";
 
 export default function Footer() {
   return (
-    <footer>
-      <section>
-        <div>
-          <h4>Contact</h4>
+    <footer className="w-screen bg-brandTeal-900 text-white ">
+      <div className="flex justify-between flex-col sm:flex-row gap-8 w-10/12 mx-auto py-16">
+        <section className="">
+          <div>
+            <h4 className="text-xl mb-2">Contact</h4>
+            <ul>
+              <li className="mb-4">
+                <a href="mailto:contact@jayeducation.co.uk">
+                  contact@jayeduction.co.uk
+                </a>
+              </li>
+              <li>
+                <Image src={facebook} alt="Facebook Logo" width={32} />
+              </li>
+            </ul>
+          </div>
+        </section>
+        <section className="">
+          <h4 className="text-xl mb-2">Quick Links</h4>
           <ul>
             <li>
-              <a href="mailto:contact@jayeducation.co.uk">
-                contact@jayeduction.co.uk
-              </a>
+              <Link className="underline" href={"/about"}>
+                About
+              </Link>
             </li>
             <li>
-              <Image src={facebook} alt="Facebook Logo" width={32} />
+              <Link className="underline" href={"/contact"}>
+                Contact
+              </Link>
+            </li>
+            <li>
+              <Link className="underline" href={"/contact"}>
+                Tuition Services
+              </Link>
+            </li>
+            <li>
+              <Link className="underline" href={"/contact"}>
+                Workshops
+              </Link>
             </li>
           </ul>
-        </div>
-        <div>
-          <h4>Opening Hours</h4>
+        </section>
+        <section className="">
+          <h4 className="text-xl mb-2">Downloads</h4>
           <ul>
             <li>
-              <p>Weekdays, 8am - 8pm</p>
+              <Link
+                className="underline"
+                href={"/downloads/JAYEducation_GuidanceforOnlineSessions.pdf"}
+                target="_blank"
+                download
+              >
+                Guidance for Online Sessions
+              </Link>
             </li>
             <li>
-              <p>Saturdays, 8am - 3pm</p>
+              <Link
+                className="underline"
+                href={"/downloads/JAYEducation_TermsandConditions.pdf"}
+                target="_blank"
+                download
+              >
+                Terms and Conditions
+              </Link>
+            </li>
+            <li>
+              <Link
+                className="underline"
+                href={"/downloads/JAYEducation_PrivacyPolicy.pdf"}
+                target="_blank"
+                download
+              >
+                Privacy Policy
+              </Link>
+            </li>
+            <li>
+              <Link
+                className="underline"
+                href={"/downloads/JAYEducation_SafeguardingStatement.pdf"}
+                target="_blank"
+                download
+              >
+                Safeguarding Statement
+              </Link>
             </li>
           </ul>
-        </div>
-      </section>
-      <section>
-        <h4>Downloads</h4>
-        <ul>
-          <li>
-            <Link
-              href={"/downloads/JAYEducation_GuidanceforOnlineSessions.pdf"}
-              target="_blank"
-              download
-            >
-              Guidance for Online Sessions
-            </Link>
-          </li>
-          <li>
-            <Link
-              href={"/downloads/JAYEducation_TermsandConditions.pdf"}
-              target="_blank"
-              download
-            >
-              Terms and Conditions
-            </Link>
-          </li>
-          <li>
-            <Link
-              href={"/downloads/JAYEducation_PrivacyPolicy.pdf"}
-              target="_blank"
-              download
-            >
-              Privacy Policy
-            </Link>
-          </li>
-          <li>
-            <Link
-              href={"/downloads/JAYEducation_SafeguardingStatement.pdf"}
-              target="_blank"
-              download
-            >
-              Safeguarding Statement
-            </Link>
-          </li>
-        </ul>
-      </section>
+        </section>
+      </div>
+      <div className="text-center bg-black text-white">
+        <p className="text-sm py-2">
+          web design by{" "}
+          <a className="underline" href="https://samueledraper.com/">
+            samuel draper
+          </a>
+        </p>
+      </div>
     </footer>
   );
 }
